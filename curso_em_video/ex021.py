a7aa28b00e250280894fcfbf8117c350339dd8df
yzakius <isaacmob@gmail.com>
2019-06-16 17:09:19 -0300

    Adicionados exercícios do 21 ao 30 do curso em vídeo

--- curso_em_video/ex021.py
+++ curso_em_video/ex021.py
@@ -1,6 +1,6 @@
 #Solução do professor: uso do pygame
 import pygame
 pygame.init() #necessário para iniciar
-pygame.mixer.music.load('your_mp3_here.mp3')
+pygame.mixer.music.load('your_mp3_file_here.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
